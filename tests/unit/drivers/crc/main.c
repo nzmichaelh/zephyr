96f3672d91554025d9f47f2459e76e7a7795499e
Michael Hope <mlhx@google.com>
2017-11-29 22:26:16 +0100

    drivers: add a crc7 interface and software implementation.
    crc7 is used by the MMC/SD card interface and the WINC1500 Wifi
    controller.  This adds a bytewise software implementation which is
    reasonably fast and significantly more compact than a syndrome based
    implementation.
    
    The API matches Linux.  The test cases match an example SD card
    packet,
    http://reveng.sourceforge.net/crc-catalogue/1-15.htm#crc.cat-bits.7,
    and give the same output as the Linux version.

--- tests/unit/drivers/crc/main.c
+++ tests/unit/drivers/crc/main.c
@@ -7,6 +7,7 @@
 #include <ztest.h>
 
 #include <drivers/crc/crc16_sw.c>
+#include <drivers/crc/crc7_sw.c>
 
 void test_crc16(void)
 {
@@ -19,8 +20,24 @@ void test_crc16(void)
 	zassert(crc16_ccitt(test2, sizeof(test2)) == 0xe5cc, "pass", "fail");
 }
 
+void test_crc7(void)
+{
+	u8_t test0[] = { };
+	u8_t test1[] = { 'A' };
+	u8_t test2[] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+	u8_t test3[] = { 0x77, 0, 0, 0, 0 };
+
+	zassert_equal(crc7(0, test0, sizeof(test0)), 0, NULL);
+	zassert_equal(crc7(0, test1, sizeof(test1)), 0x6d, NULL);
+	zassert_equal(crc7(0, test2, sizeof(test2)), 0x75, NULL);
+	zassert_equal(crc7(0, test3, sizeof(test3)), 0x32, NULL);
+}
+
 void test_main(void)
 {
-	ztest_test_suite(test_crc16, ztest_unit_test(test_crc16));
+	ztest_test_suite(test_crc16,
+			 ztest_unit_test(test_crc16),
+			 ztest_unit_test(test_crc7)
+			 );
 	ztest_run_test_suite(test_crc16);
 }
