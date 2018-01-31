a1701b29cdcbdd44e2f582cb33964519751d2864
Michael Hope <mlhx@google.com>
2018-01-31 21:06:08 +0100

    boards: enable the SPI ports on mysensors_gw.
    The board has two SPI ports: one shared across the radio and SD card
    and one for others.
    
    Signed-off-by: Michael Hope <mlhx@google.com>

--- boards/arm/mysensors_gw/pinmux.c
+++ boards/arm/mysensors_gw/pinmux.c
@@ -10,6 +10,7 @@
 static int board_pinmux_init(struct device *dev)
 {
 	struct device *muxa = device_get_binding(CONFIG_PINMUX_SAM0_A_LABEL);
+	struct device *muxb = device_get_binding(CONFIG_PINMUX_SAM0_B_LABEL);
 
 	ARG_UNUSED(dev);
 
@@ -32,6 +33,33 @@ static int board_pinmux_init(struct device *dev)
 #error Pin mapping is not configured
 #endif
 #if CONFIG_UART_SAM0_SERCOM5_BASE_ADDRESS
+#error Pin mapping is not configured
+#endif
+
+#if CONFIG_SPI_SAM0_SERCOM1_BASE_ADDRESS
+	/* SPI1 on MOSI=PA16/pad 0, MISO on PA18/pad 2, SCK on PA17/pad 1 */
+	pinmux_pin_set(muxa, 16, PINMUX_FUNC_C);
+	pinmux_pin_set(muxa, 18, PINMUX_FUNC_C);
+	pinmux_pin_set(muxa, 17, PINMUX_FUNC_C);
+#endif
+
+#if CONFIG_SPI_SAM0_SERCOM4_BASE_ADDRESS
+	/* SPI4 on MOSI=PB12/pad 0, MISO on PB14/pad 2, SCK on PB13/pad 1 */
+	pinmux_pin_set(muxb, 12, PINMUX_FUNC_C);
+	pinmux_pin_set(muxb, 14, PINMUX_FUNC_C);
+	pinmux_pin_set(muxb, 13, PINMUX_FUNC_C);
+#endif
+
+#if CONFIG_SPI_SAM0_SERCOM0_BASE_ADDRESS
+#error Pin mapping is not configured
+#endif
+#if CONFIG_SPI_SAM0_SERCOM2_BASE_ADDRESS
+#error Pin mapping is not configured
+#endif
+#if CONFIG_SPI_SAM0_SERCOM3_BASE_ADDRESS
+#error Pin mapping is not configured
+#endif
+#if CONFIG_SPI_SAM0_SERCOM5_BASE_ADDRESS
 #error Pin mapping is not configured
 #endif
 
