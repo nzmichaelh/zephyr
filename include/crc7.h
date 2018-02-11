fa8221f1960bcfb47dfc3f6300361832284d2b38
Michael Hope <mlhx@google.com>
2018-02-11 21:00:24 +0100

    crc7: add testcases.

--- include/crc7.h
+++ include/crc7.h
@@ -18,7 +18,8 @@ extern "C" {
  * @brief Compute the CRC-7 checksum of a buffer.
  *
  * See JESD84-A441.  Used by the MMC protocol.  Uses 0x09 as the
- * polynomial with no reflection.
+ * polynomial with no reflection.  The CRC is left
+ * justified, so bit 7 of the result is bit 6 of the CRC.
  *
  * @param seed Value to seed the CRC with
  * @param src Input bytes for the computation
@@ -26,7 +27,7 @@ extern "C" {
  *
  * @return The computed CRC7 value
  */
-u8_t crc7(u8_t seed, const u8_t *src, size_t len);
+u8_t crc7_be(u8_t seed, const u8_t *src, size_t len);
 
 #ifdef __cplusplus
 }
--- lib/crc/crc7_sw.c
+++ lib/crc/crc7_sw.c
@@ -6,26 +6,13 @@
 
 #include <crc7.h>
 
-u8_t crc7(u8_t seed, const u8_t *src, size_t len)
+u8_t crc7_be(u8_t seed, const u8_t *src, size_t len)
 {
 	while (len--) {
-		u8_t ch = *src++;
-		u8_t d0 = ch & 1;
+		u8_t e = seed ^ *src++;
+		u8_t f = e ^ (e >> 4) ^ (e >> 7);
 
-		u8_t e = seed ^ (ch >> 1);
-
-		u8_t e345 = (e >> 3) & 7;
-		u8_t e01 = e & 3;
-		u8_t e6 = (e >> 6) & 1;
-		u8_t e2 = (e >> 2) & 1;
-
-		u8_t t = e345 ^ (e01 << 1) ^ d0;
-
-		u8_t t1 = t ^ (t << 3);
-		u8_t t2 = e6 ^ (e2 << 3) ^ ((e6 ^ e2) << 6);
-		u8_t t3 = e345 << 4;
-
-		seed = t1 ^ t2 ^ t3;
+		seed = (f << 1) ^ (f << 4);
 	}
 
 	return seed;
