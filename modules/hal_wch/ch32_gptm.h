9740ee62823571e1317817bcb672d960c9b99429
Michael Hope <mlhx@google.com>
2024-06-24 20:02:18 +0200

    drivers: add the ch32v00x PWM driver
    Signed-off-by: Michael Hope <mlhx@google.com>

---
+++ modules/hal_wch/ch32_gptm.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2024 Google LLC.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _CH32_GPTM_H
+#define _CH32_GPTM_H
+
+#ifdef CONFIG_SOC_CH32V003
+#include <ch32v003fun.h>
+#else
+#error "SoC not supported!"
+#endif
+
+#endif
