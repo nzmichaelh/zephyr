7f261d4fa30d2813f8193eed9af5f119ae9e037e
Michael Hope <michaelh@juju.nz>
2025-05-12 18:12:32 +0000

    samples: counter: add the ch32v006evt to the alarm sample
    Signed-off-by: Michael Hope <michaelh@juju.nz>

--- samples/drivers/counter/alarm/src/main.c
+++ samples/drivers/counter/alarm/src/main.c
@@ -65,6 +65,8 @@ struct counter_alarm_cfg alarm_cfg;
 #define TIMER DT_NODELABEL(counter0)
 #elif defined(CONFIG_COUNTER_RENESAS_RZ_GTM)
 #define TIMER DT_INST(0, renesas_rz_gtm_counter)
+#elif defined(CONFIG_COUNTER_WCH_GPTM)
+#define TIMER DT_INST(0, wch_gptm)
 #else
 #error Unable to find a counter device node in devicetree
 #endif
